7baa57e16d91a6c9612ef067b390916241edd3f3
Ибрагимов Мурадбек <mz_ibragimov@controlsystems.ru>
2024-10-05 12:58:49 +0300

    [component] add some changes

--- src/app/app.config.browser.ts
+++ src/app/app.config.browser.ts
@@ -1,9 +1,12 @@
+import 'hammerjs';
+
 import { ApplicationConfig, mergeApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { appConfig } from './app.config';
+import { provideHammer } from '@st/core';
 
 const browserConfig: ApplicationConfig = {
-  providers: [provideAnimations()],
+  providers: [provideAnimations(), provideHammer()],
 };
 
 export const config = mergeApplicationConfig(appConfig, browserConfig);
