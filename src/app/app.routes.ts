04ff1549067204155b0488cd5d5c7668af91e8e0
Ибрагимов Мурадбек <mz_ibragimov@controlsystems.ru>
2024-10-08 16:59:16 +0300

    [layout] add layout, header, footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () => import('@st/ui/layout').then((m) => m.LayoutComponent),
+    children: [
+      {
+        path: '',
+        loadComponent: () => import('@st/ui/layout').then((m) => m.HeaderComponent),
+        outlet: 'header',
+      },
+      {
+        path: '',
+        loadComponent: () => import('@st/ui/layout').then((m) => m.FooterComponent),
+        outlet: 'footer',
+      },
+    ],
+  },
+];
